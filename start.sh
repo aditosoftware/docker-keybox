fc3032e6b9fb29a779ae3ca23d6128b16d070cdc
Marcel Moldaschl <m.moldaschl@adito.de>
2015-10-20 17:38:01 +0200

    optimized  if statement

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-if [ -f '/a/data/KeyBoxConfig.properties' ]
+set -x
+if [ ! -f '/a/data/KeyBoxConfig.properties' ]
 then
-    ln -sf /a/data/KeyBoxConfig.properties /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/KeyBoxConfig.properties
-else
     cp /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/KeyBoxConfig.properties /a/data/KeyBoxConfig.properties
-    ln -sf /a/data/KeyBoxConfig.properties /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/KeyBoxConfig.properties
 fi
+ln -sf /a/data/KeyBoxConfig.properties /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/KeyBoxConfig.properties
 
 mkdir -p /a/data/keydb
 rm -rf /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/keydb
 ln -sf /a/data/keydb /opt/KeyBox-jetty/jetty/keybox/WEB-INF/classes/
-
 
 cd /opt/KeyBox-jetty/jetty/
 java -Xmx1024m -jar start.jar
